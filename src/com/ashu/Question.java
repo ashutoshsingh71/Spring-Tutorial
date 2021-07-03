7977f3ecc05c2ca9524d30918cab139494438922
ashutoshsingh71 <ashutoshbit71@gmail.com>
2021-07-04 00:17:17 +0530

    CI with map collections

--- src/com/ashu/Question.java
+++ src/com/ashu/Question.java
@@ -2,13 +2,15 @@ package com.ashu;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Question {
     int id;
     String name;
-    List<Answer> answers;
+    Map<String,String> answers;
 
-    public Question(int id, String name, List<Answer> answers) {
+    public Question(int id, String name, Map<String,String> answers) {
         this.id = id;
         this.name = name;
         this.answers = answers;
@@ -30,20 +32,22 @@ public class Question {
         this.name = name;
     }
 
-    public List<Answer> getAnswers() {
+    public Map<String,String> getAnswers() {
         return answers;
     }
 
-    public void setAnswers(List<Answer> answers) {
+    public void setAnswers(Map<String,String> answers) {
         this.answers = answers;
     }
 
     public void display(){
         System.out.println("Question: " + this.name);
         System.out.println("Answers are: ");
-        Iterator<Answer> iterator = answers.iterator();
+        Set<Map.Entry<String,String>> entry = answers.entrySet();
+        Iterator<Map.Entry<String,String>> iterator = entry.iterator();
         while (iterator.hasNext()){
-            System.out.println(iterator.next());
+            Map.Entry<String,String> entrySet = iterator.next();
+            System.out.println("Answer: " + entrySet.getKey() + " " + "Posted By: " + entrySet.getValue());
         }
     }
 }
